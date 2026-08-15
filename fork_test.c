27b90f62bfd5eb2ee89cf2bb839d736ce76fc466
Birits <Birits>
2026-08-15 14:36:36 -0500

    Create shell

--- fork_test.c
+++ fork_test.c
@@ -1,20 +1,58 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
-  printf("Before fork, my PID is %d\n", getpid());
+  const int SIZE = 100;
 
-  pid_t result = fork();
+  char buffer[SIZE];
+  char *argv[SIZE];
+  const char *delimiters = "  ";
+  int counter = 0;
 
-  if (result == 0) {
-    printf("I'm the child, my PID is %d, fork() returned %d\n", getpid(),
-           result);
-  } else if (result > 0) {
-    printf(
-        "I'm the parent, my PID is %d, fork() returned %d (my child's PID)\n",
-        getpid(), result);
-  } else {
-    printf("fork() failed\n");
+  while (true) {
+    counter = 0;
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+      return -1;
+    }
+    buffer[strcspn(buffer, "\n")] = '\0';
+
+    char *token = strtok(buffer, delimiters);
+    if (token == NULL) {
+      continue;
+    }
+
+    while (token != NULL) {
+      printf("Token: %s\n", token);
+      argv[counter] = token;
+      token = strtok(NULL, delimiters);
+      counter++;
+    }
+    argv[counter] = NULL;
+    
+    if (strcmp(argv[0], "exit") == 0) {
+      break;
+    }
+
+    printf("Before fork, my PID is %d\n", getpid());
+
+    pid_t result = fork();
+
+    if (result == 0) {
+      printf("I'm the child, my PID is %d, fork() returned %d\n", getpid(),
+             result);
+      execvp(argv[0], argv);
+      return -1;
+    } else if (result > 0) {
+      wait(NULL);
+      printf(
+          "I'm the parent, my PID is %d, fork() returned %d (my child's PID)\n",
+          getpid(), result);
+    } else {
+      printf("fork() failed\n");
+    }
   }
 
   return 0;
